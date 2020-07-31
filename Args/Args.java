8bc5b3112efdc966a574f5377d8dbb15da98ee60
Shulker233Git <ShulkerWalker>
2020-07-31 03:32:30 +0000

    Args pushed

---
+++ Args/Args.java
@@ -0,0 +1,10 @@
+public class Args {
+	public static void main(String[] args) {
+		if(args[0].length=0) {
+			System.out.println("Args-ShulkeeOliverChin    ");
+		}
+		else {
+			System.out.println("Args-ShulkerOliverChin    "+args[0]);
+		}
+	}
+}
