298acb937883299157b8318c0990559c682e96a3
Shulker233Git <ShulkerWalker>
2020-08-01 14:14:14 +0000

    Args update, You can try to enter a parameter when run the code.

--- Args/Args.java
+++ Args/Args.java
@@ -1,10 +1,11 @@
 public class Args {
 	public static void main(String[] args) {
+		System.out.println("[Args]: Welcome to the Args parameter repertours");
 		if(args.length==0) {
-			System.out.println("Args-ShulkeeOliverChin    ");
+			System.out.println("[Args]: Please enter a parameter");
 		}
 		else {
-			System.out.println("Args-ShulkerOliverChin    "+args[0]);
+			System.out.println("[Args]: "+args[0]);
 		}
 	}
 }
