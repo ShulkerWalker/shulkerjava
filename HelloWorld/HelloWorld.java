e43fa1dfc5cfc2e35d9e37460984ff3a1e5db972
Shulker233Git <ShulkerWalker>
2020-08-02 13:55:10 +0000

    All code standardization

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,6 @@
+//ShulkerOliverChin-Project-0
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld-ShulkerOliverChin");
 	}
 }
-//ShulkerOliverChin
---
+++ SSHConnectionSelector/scs.java
@@ -0,0 +1,7 @@
+//ShulkerOliverChin-Project-2
+public class scs {
+	public static void main(String[] args) {
+		System.out.println("[CSC]Welcome to SSHConnectionSelector");
+		System.out.println("[CSC]Loading config profile");
+	}
+}
