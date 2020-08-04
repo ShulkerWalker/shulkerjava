787aecd58789b9d38331e490b2e0b0efdb1a2061
Shulker233Git <ShulkerWalker>
2020-08-04 00:54:58 +0000

    update

--- SSHConnectionSelector/scs.java
+++ SSHConnectionSelector/scs.java
@@ -1,7 +1,19 @@
 //ShulkerOliverChin-Project-2
+import java.io.File;
 public class scs {
 	public static void main(String[] args) {
 		System.out.println("[CSC]Welcome to SSHConnectionSelector");
-		System.out.println("[CSC]Loading config profile");
+		File file=new File("./scs_config/");
+		File file1=new File("./scs_config/configs");
+		try {
+		}	catch (Object e) {
+			if(!isDirectory()) {
+				file.mkdir();
+				file1.createNewFile();
+			}
+			else {
+				file1.createNewFile();
+			}
+		}
 	}
 }
