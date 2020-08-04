b6cf7dc655a183023c6aa558105514452fc2b75e
Shulker233Git <ShulkerWalker>
2020-08-04 12:50:27 +0000

    StartDust Mua~w~

---
+++ StarDust/learn.java
@@ -0,0 +1,12 @@
+//ShulkerOliverChin-Project-1
+public class learn {
+	public static void main(char[] args) {
+		System.out.println("[Args]: Welcome to the Args parameter repeater");
+		if(args.length==0) {
+			System.out.println("[Args]: Please enter a parameter");
+		}
+		else {
+			System.out.println("[Args]: "+args[0]);
+		}
+	}
+}
