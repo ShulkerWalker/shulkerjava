272d1b3f07977333578a1cb2b0618938826cdb2d
ShulkerWalker <2531493755@qq.com>
2020-09-01 15:06:04 +0800

    SCS Shell

--- SSHConnectionSelector/com/shulker/scs/scs.java
+++ SSHConnectionSelector/com/shulker/scs/scs.java
@@ -20,12 +20,39 @@ public class scs {
 		}
 		if (args.length > 0){
 			if (args[0].equals("add")){
-				if (args.length == 4){
-					System.out.println("[SCS]Add server's config successfully "+ args[1] + "@" + args[2] + ":" + args[3]);
+				if (args.length == 5){
+					System.out.println("[SCS]Add server's config successfully <" + args[1] + ">" + args[2] + "@" + args[3] + ":" + args[4]);
 				}
 				else {
-					System.out.println("[SCS]Usage: add <Account> <IP> <Port>");
+					System.out.println("[SCS]Usage: add <Name> <Account> <IP> <Port>");
 				}
+			}
+			else if (args[0].equals("delete")){
+				if(args.length == 2){
+					System.out.println("[SCS]Delete server's config successfully <" + args[1] + ">");
+				}
+				else {
+					System.out.println("[SCS]Usage: delete <Name>");
+				}
+			}
+			else if (args[0].equals("ssh")){
+				if (args.length == 2){
+					System.out.println("[SCS]Ssh connection " + "<" + args[1] + ">" + "start");
+				}
+				else {
+					System.out.println("[SCS]Usage: ssh <Name>");
+				}
+			}
+			else if (args[0].equals("sftp")){
+				if (args.length == 2){
+					System.out.println("[SCS]Sftp connection " + "<" + args[1] + ">" + "start");
+				}
+				else {
+					System.out.println("[SCS]Usage: sftp <Name>");
+				}
+			}
+			else if (args[0].equals("list")){
+				System.out.println("[SCS]Configurations list");
 			}
 			else {
 				System.out.println("[SCS]Please enter a option");
@@ -36,6 +63,6 @@ public class scs {
 			System.out.println("[SCS]Please enter a option");
 			System.out.println("[SCS]<add> <delete> <ssh> <sftp> <list>");
 		}
-	}
+    }
 }
 	
